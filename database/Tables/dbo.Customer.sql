4e0996b77fa398911ab33ee97045bfd7723ae371
xSlothx <xSlothx>
2021-11-11 09:42:12 -0800

    Update table

--- database/Tables/dbo.Customer.sql
+++ database/Tables/dbo.Customer.sql
@@ -16,6 +16,7 @@ CREATE TABLE [dbo].[Customer] (
 		[Test]           [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[Test2]          [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[Test3]          [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[Test6]          [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		CONSTRAINT [Customer_UK]
 		UNIQUE
 		NONCLUSTERED
