4c02ad5e941063df36655c987dcd6177aa795c43
xSlothx <xSlothx>
2021-11-11 16:33:32 -0800

    ApexSQL Audit Triggers

--- database/Procedures/dbo.GetCustomerByName.sql
+++ database/Procedures/dbo.GetCustomerByName.sql
@@ -1,5 +1,17 @@
 SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
 GO
---Procedure [dbo].[GetCustomerByName] is encrypted. It could not be scripted
+
+CREATE PROCEDURE dbo.GetCustomerByName @custname varchar(70)
+ 
+AS
+SELECT 
+    c.FirstName, c.LastName, c.Phone, c.City
+FROM dbo.Customer c
+WHERE c.FirstName = @custname;
+
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'TEST3', 'SCHEMA', N'dbo', 'PROCEDURE', N'GetCustomerByName', NULL, NULL
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'TEST2', 'SCHEMA', N'dbo', 'PROCEDURE', N'GetCustomerByName', 'PARAMETER', N'@custname'
 GO
