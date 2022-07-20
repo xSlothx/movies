ece55e3e30689f0bfa9a9c0de0d21c59f33846f9
xSlothx <xSlothx>
2022-07-20 15:57:40 -0700

    Added col4 to Feature 2

--- database/Tables/dbo.Customer2.sql
+++ database/Tables/dbo.Customer2.sql
@@ -16,6 +16,7 @@ CREATE TABLE [dbo].[Customer2] (
 		[Test]           [date] NULL,
 		[Test2]          [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[Test3]          [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[Test4]          [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		CONSTRAINT [UQ__Customer__BA0BD0C5ECB0B82F]
 		UNIQUE
 		NONCLUSTERED
