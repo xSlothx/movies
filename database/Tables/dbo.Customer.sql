7c91a305f8d3dd53254c03091df30b7b978626b5
xSlothx <xSlothx>
2023-04-05 08:22:48 -0700

    New changes

--- database/Tables/dbo.Customer.sql
+++ database/Tables/dbo.Customer.sql
@@ -17,6 +17,9 @@ CREATE TABLE [dbo].[Customer] (
 		[Test2]          [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[Test3]          [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[Test4]          [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[test5]          [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[Test6]          [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[Test7]          [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		CONSTRAINT [Customer_UK]
 		UNIQUE
 		NONCLUSTERED
