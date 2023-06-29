ae881bec8f54590584cc821b001099ece2616c1f
xSlothx <xSlothx>
2023-06-29 08:28:09 -0700

    Auto Build

--- database/Tables/dbo.Customer.sql
+++ database/Tables/dbo.Customer.sql
@@ -27,6 +27,7 @@ CREATE TABLE [dbo].[Customer] (
 		[Test12]         [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[Test13]         [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[Test14]         [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[test15]         [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		CONSTRAINT [Customer_UK]
 		UNIQUE
 		NONCLUSTERED
