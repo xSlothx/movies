dcee02348e2a74c059fcc447fe0d6d8aec89fdff
xSlothx <xSlothx>
2023-12-13 08:45:54 -0800

    Mat updates table

--- database/Tables/dbo.Customer.sql
+++ database/Tables/dbo.Customer.sql
@@ -14,6 +14,7 @@ CREATE TABLE [dbo].[Customer] (
 		[BirthDate]      [date] NULL,
 		[Gender]         [char](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[Test]           [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[Test2]          [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		CONSTRAINT [Customer_UK]
 		UNIQUE
 		NONCLUSTERED
