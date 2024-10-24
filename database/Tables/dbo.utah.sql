49b9384ddea9552e1a53737f2384e7c36d24502d
xSlothx <35970992+xSlothx@users.noreply.github.com>
2024-10-24 10:37:54 -0700

    Update dbo.utah.sql
    Update utah table

--- database/Tables/dbo.utah.sql
+++ database/Tables/dbo.utah.sql
@@ -11,7 +11,7 @@ CREATE TABLE [dbo].[utah] (
 		[SAL]          [int] NULL,
 		[COMM]         [int] NULL,
 		[DEPTNO]       [int] NULL,
-		[Test1]        [nchar](12) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+		[Test1]        [nchar](15) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[utah] SET (LOCK_ESCALATION = TABLE)
