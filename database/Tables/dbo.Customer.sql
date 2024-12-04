ae6d2d8f3d54e915bc2950968d6eac20782a3a44
xSlothx <xSlothx>
2024-12-04 13:21:49 -0800

    Mat updated customer table with new column

--- database/Tables/dbo.Customer.sql
+++ database/Tables/dbo.Customer.sql
@@ -20,6 +20,7 @@ CREATE TABLE [dbo].[Customer] (
 		[Test5]          [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[Test6]          [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[Test7]          [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[Test8]          [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		CONSTRAINT [Customer_UK]
 		UNIQUE
 		NONCLUSTERED
